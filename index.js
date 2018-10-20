694eefe97cb7bd6ae5bd8ded2c770a25a66f595c
global-keyboard-5117 <stacey.cupid@gmail.com>
2018-10-20 04:02:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var object 
   object[key] = value
   return object
 }
